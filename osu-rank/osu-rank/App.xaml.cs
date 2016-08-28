2d1b8e286ca1be103bcbd88a03fda51a80c1e254
Jérémy <blackrockshooter76@outlook.fr>
2016-08-28 18:42:16 +0200

    Fixed "TextBox" in key prompt on startup (fixes issue #1)

--- osu-rank/osu-rank/App.xaml.cs
+++ osu-rank/osu-rank/App.xaml.cs
@@ -19,7 +19,7 @@ namespace osurank
         public static readonly CultureInfo systemCulture = CultureInfo.CurrentCulture;
         // Needed for update logic
         public static bool HasCheckedForUpdates = false;
-        public static int version = 19;
+        public static int version = 20;
 
         private void App_Startup(object sender, StartupEventArgs e)
         {
