692ac5971622d5af7140216bdc583c3b466501b0
Jérémy <blackrockshooter76@outlook.fr>
2016-08-28 21:10:07 +0200

    Created content for About page

--- osu-rank/osu-rank/About.xaml.cs
+++ osu-rank/osu-rank/About.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -27,6 +28,31 @@ namespace osurank
             about.Content = "osu!rank " + Assembly.GetExecutingAssembly().GetName().Version.Major 
                                         + "." + Assembly.GetExecutingAssembly().GetName().Version.Minor 
                                         + "." + Assembly.GetExecutingAssembly().GetName().Version.Build;
+        }
+
+        private void page_loaded(object sender, RoutedEventArgs e)
+        {
+            RenderOptions.SetBitmapScalingMode(osuSmall, BitmapScalingMode.HighQuality);
+        }
+
+        private void goTopic_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://osu.ppy.sh/forum/t/478865");
+        }
+
+        private void goGeetHub_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/Jeremiidesu/osu-rank");
+        }
+
+        private void reportBug_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/Jeremiidesu/osu-rank/issues");
+        }
+
+        private void goContribute_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/Jeremiidesu/osu-rank/tree/master/osu-rank/osu-rank/Stringlists");
         }
     }
 }
