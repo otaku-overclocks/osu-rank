otaku-overclocks/osu-rank

commit 69d66ae2daf690e2df8849a476bc84e92d1cfd37

Fixed. (close #6)

osu-rank/osu-rank/OneUser.xaml.cs
@@ -55,6 +55,7 @@ namespace osurank
         private void fetchUserData(string player_name, int gamemode_index, bool show_errors = true)
         {
             dynamic userdata = osu.GetUser(player: player_name, gamemode: gamemode_index, apikey: Settings.Default.apikey, showErrors:show_errors)[0];
+            if (userdata == null){return;}
             playername.Content = Convert.ToString(userdata.username);
             Avatar.Source = new ImageSourceConverter().ConvertFromString("http://a.ppy.sh/" + userdata.user_id) as ImageSource;
             globalrank.Content = "#" + userdata.pp_rank;

osu-rank/osu-rank/osu.cs
@@ -21,7 +21,7 @@ namespace osurank
                     if (showErrors==true) MessageBox.Show(Tx.T("errors.Does not exist", "name", player), Tx.T("errors.Error"), MessageBoxButton.OK, MessageBoxImage.Error);
                     return null;
                 }
-                else if (Convert.ToString(userdata[0].pp_rank) == "null")
+                else if (userdata[0].pp_rank == null)
                 {
                     if (showErrors == true) MessageBox.Show(Tx.T("errors.Did not play yet", "name", player), Tx.T("errors.Error"), MessageBoxButton.OK, MessageBoxImage.Exclamation);
                     return null;

osu-rank/osu-rank/Compare.xaml.cs
@@ -61,12 +61,14 @@ namespace osurank
             diff_totalscore.Content = "0";
             diff_rankedscore.Content = "0";
         }
-
         private void fetchUserData(string player_name1, string player_name2, int gamemode_index, bool show_errors = true)
         {
             // retrieve userdata
             dynamic userdata1 = osu.GetUser(player: player_name1, gamemode: gamemode_index, apikey: Settings.Default.apikey, showErrors:show_errors);
+            dynamic userdata2 = osu.GetUser(player: player_name2, gamemode: gamemode_index, apikey: Settings.Default.apikey);
             dynamic player1 = null;
+            dynamic player2 = null;
+
             if (userdata1[0]!=null)
             {
                 player1 = userdata1[0];
@@ -81,9 +83,6 @@ namespace osurank
             }
             
            
-            
-            dynamic userdata2 = osu.GetUser(player: player_name2, gamemode: gamemode_index, apikey: Settings.Default.apikey);
-            dynamic player2 = null;
             if (userdata2[0] != null)
             {
                 player2 = userdata2[0];

commit d5b1511449424eb10e15df119700ebf28bc4655b

Fixed exception/regression from 69d66ae

osu-rank/osu-rank/OneUser.xaml.cs
@@ -55,7 +55,7 @@ namespace osurank
         private void fetchUserData(string player_name, int gamemode_index, bool show_errors = true)
         {
             dynamic userdata = osu.GetUser(player: player_name, gamemode: gamemode_index, apikey: Settings.Default.apikey, showErrors:show_errors)[0];
-            if (userdata == null){return;}
+            if (!(userdata is Newtonsoft.Json.Linq.JObject)) {return;}
             playername.Content = Convert.ToString(userdata.username);
             Avatar.Source = new ImageSourceConverter().ConvertFromString("http://a.ppy.sh/" + userdata.user_id) as ImageSource;
             globalrank.Content = "#" + userdata.pp_rank;

osu-rank/osu-rank/osu.cs
@@ -24,7 +24,7 @@ namespace osurank
                 else if (userdata[0].pp_rank == null)
                 {
                     if (showErrors == true) MessageBox.Show(Tx.T("errors.Did not play yet", "name", player), Tx.T("errors.Error"), MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                    return null;
+                    return " ";
                 }
                 else return userdata;
             }