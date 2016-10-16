c7d70736e6d44544953e5186caccbc6773e5d62e
Jérémy <blackrockshooter76@outlook.fr>
2016-10-16 20:47:58 +0200

    Started logic for displaying stat changes for one player

--- osu-rank/osu-rank/OneUser.xaml.cs
+++ osu-rank/osu-rank/OneUser.xaml.cs
@@ -32,6 +32,10 @@ namespace osurank
         NumberFormatInfo NFIperformance = (NumberFormatInfo)CultureInfo.CurrentCulture.NumberFormat.Clone();
         string username = Settings.Default.DefaultPlayer;
         int gamemode = Settings.Default.DefaultGamemode;
+        dynamic userdata;
+        // variables for variation labels
+        string previousUsername;
+        dynamic previousRefresh;
         // ------ END VARIABLES ------
 
         private void autoRefresh_Tick(object sender, EventArgs e)
@@ -51,11 +55,16 @@ namespace osurank
             gamemode = gamemodeDropdown.SelectedIndex;
             fetchUserData(nameInput.Text, gamemodeDropdown.SelectedIndex);
         }
-        
         private void fetchUserData(string player_name, int gamemode_index, bool show_errors = true)
         {
-            dynamic userdata = osu.GetUser(player: player_name, gamemode: gamemode_index, apikey: Settings.Default.apikey, showErrors:show_errors)[0];
-
+            bool samename = false;
+            if (player_name==previousUsername)
+            {
+                previousRefresh = userdata;
+                samename = true;
+            }
+            userdata = osu.GetUser(player: player_name, gamemode: gamemode_index, apikey: Settings.Default.apikey, showErrors:show_errors)[0];
+            previousUsername = player_name;
             // do nothing if player did not play or player is invalid
             if (Convert.ToString(userdata) == Convert.ToString(int.MaxValue) ) { return; }
             if (userdata.pp_rank == null) {return;}
@@ -72,17 +81,54 @@ namespace osurank
             pp.Content = Convert.ToDouble(userdata.pp_raw).ToString("n", NFIperformance) + " pp";
             totalScore.Content = Tx.TC("player.Total score") + " " + Convert.ToInt64(userdata.total_score).ToString("n", NFInoDecimal);
             level.Content = Tx.TC("player.Level") + " " + Convert.ToDouble(userdata.level).ToString("n", NFInoDecimal) + " (" + ((Convert.ToDouble(userdata.level) - Math.Truncate(Convert.ToDouble(userdata.level))) * 100).ToString("n", NFInoDecimal) + "%)";
-            grades.Content = "SS / S / A: " + Convert.ToInt32(userdata.count_rank_ss).ToString("n", NFInoDecimal) + " / "
+            /* grades.Content = "SS / S / A: " + Convert.ToInt32(userdata.count_rank_ss).ToString("n", NFInoDecimal) + " / "
                                                 + Convert.ToInt32(userdata.count_rank_s).ToString("n", NFInoDecimal) + " / "
-                                                + Convert.ToInt32(userdata.count_rank_a).ToString("n", NFInoDecimal);
+                                                + Convert.ToInt32(userdata.count_rank_a).ToString("n", NFInoDecimal); */
+            SS.Content = Convert.ToInt32(userdata.count_rank_ss).ToString("n", NFInoDecimal);
+            S.Content = Convert.ToInt32(userdata.count_rank_s).ToString("n", NFInoDecimal);
+            A.Content = Convert.ToInt32(userdata.count_rank_a).ToString("n", NFInoDecimal);
             userID.Content = Tx.TC("player.User ID") + " " + userdata.user_id;
             levelProgress.Value = ((Convert.ToDouble(userdata.level) - Math.Truncate(Convert.ToDouble(userdata.level))) * 100);
+            if (samename==true)
+            {
+                updateVariation(previousRefresh,userdata);
+            }
+            else
+            {
+                //insert reset code here
+            }
         }
 
         private void timerReset()
         {
             autoRefresh.Stop();
             autoRefresh.Start();
+        }
+
+        private void updateVariation(dynamic before, dynamic after)
+        {
+            // so many needed variables
+            long RscoreDiff, Scorediff;
+            int PCdiff,rankdiff,countryrankdiff,Sdiff,SSdiff,Adiff;
+            double ppDiff, accDiff;
+
+            // difference calculation 
+            RscoreDiff = (long)after.ranked_score - (long)before.ranked_score;
+            Scorediff = (long)after.total_score - (long)before.total_score;
+            PCdiff = (int)after.playcount - (int)before.playcount;
+            rankdiff = (int)before.pp_rank - (int)after.pp_rank;
+            countryrankdiff = (int)before.pp_country_rank - (int)after.pp_country_rank;
+            Sdiff = (int)after.count_rank_s - (int)before.count_rank_s;
+            SSdiff = (int)after.count_rank_ss - (int)before.count_rank_ss;
+            Adiff = (int)after.count_rank_a - (int)before.count_rank_a;
+            ppDiff = (double)after.pp_raw - (double)before.pp_raw;
+            accDiff = (double)after.accuracy - (double)before.accuracy;
+
+            // display these values
+            if (RscoreDiff > 0)
+            {
+                rankedScore_diff.Content = "+" + RscoreDiff.ToString("n", NFInoDecimal);
+            }
         }
 
         private void page_loaded(object sender, RoutedEventArgs e)
@@ -110,8 +156,10 @@ namespace osurank
                 fetchUserData(username, gamemode);
                 if (Settings.Default.RefreshEnable == true)
                 {
-                    autoRefresh.Start();
+                    autoRefresh.IsEnabled = true;
                 }
+                else
+                    autoRefresh.IsEnabled = false;
             }
         }
     }
